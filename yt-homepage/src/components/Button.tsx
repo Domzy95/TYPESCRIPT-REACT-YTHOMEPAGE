23fdaf04144b44de1aa699720e4bb977505c7ce9
Domen <domen.sinkovecgmail.com>
2024-07-23 23:05:02 +0200

    COMMIT2

--- yt-homepage/src/components/Button.tsx
+++ yt-homepage/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 //!ustvari array classov ki ga bo uporabljov vsak button
 const buttonStyles = cva(["transition-colors"], {
   variants: {
@@ -28,9 +29,14 @@ const buttonStyles = cva(["transition-colors"], {
 
 //!vzame stile buttonov ki smo jih naredili! component props omogoča da dodamo navadne button propse custom made propsom!
 type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
-export function Button({ variant, size, ...props }: ButtonProps) {
-  return <button {...props} className={buttonStyles({ variant, size })} />;
+export function Button({ variant, size, className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props} //!z tailwind merge omogočamo da overrajtamo custom made style za buttone in dodamo svoje z oznako classname
+      className={twMerge(buttonStyles({ variant, size }), className)}
+    />
+  );
 }
 //! ...props omogoča da button sprejme tudi ostale propse ki niso v buttonStyles
-//!za stylat button uporabljamo npm i class variance authority
+//!za stylat button uporabljamo npm i class variance authority z tailwind merge
 //!ker želimo imeti različne stile gumbov
--- yt-homepage/src/layouts/PageHeader.tsx
+++ yt-homepage/src/layouts/PageHeader.tsx
@@ -1,11 +1,17 @@
 import logo from "../assets/logo.png";
-import { Menu } from "lucide-react";
+import { Menu, Upload, Bell, User, Mic, Search, ArrowLeft } from "lucide-react";
 import { Button } from "../components/Button";
+import { useState } from "react";
 //! ZA IMPORT IKONE UPORABLJAMO LUCID ICONS IKONE
 export function PageHeader() {
+  const [showFullSearch, setShowFullSearch] = useState(false);
   return (
-    <div className="flex gap-20 lg:gap-20 justify-between">
-      <div className="flex gap-4 items-center flex-shrink-0">
+    <div className="flex gap-20 lg:gap-20 justify-between pt-2 mb-2 mx-4">
+      <div
+        className={`flex gap-4 items-center flex-shrink-0 ${
+          showFullSearch ? "hidden" : "flex"
+        }`}
+      >
         <Button variant="ghost" size="icon">
           <Menu />
         </Button>
@@ -13,8 +19,68 @@ export function PageHeader() {
           <img src={logo} className="h-12" alt="logo" />
         </a>
       </div>
-      <div></div>
-      <div></div>
+
+      <form
+        className={`  gap-4 flex-grow justify-center ${
+          showFullSearch ? "flex" : "hidden md:flex"
+        }`}
+      >
+        {setShowFullSearch && (
+          <Button
+            onClick={() => setShowFullSearch(false)}
+            type="button"
+            className="flex-shrink-0"
+            size="icon"
+            variant="ghost"
+          >
+            <ArrowLeft />
+          </Button>
+        )}
+        <div className="flex flex-grow max-w-[700px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="flex-shrink-0 py-2 px-2 rounded-r-full border-secondary-border">
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" className="flex-shrink-0" size="icon">
+          <Mic />
+        </Button>
+      </form>
+      <div
+        className={`flex flex-shrink-0 md-gap-2 ${
+          showFullSearch ? "hidden" : "flex"
+        }`}
+      >
+        <Button
+          onClick={() => setShowFullSearch(true)}
+          className="md:hidden"
+          size="icon"
+          variant="default"
+        >
+          <Search />
+        </Button>
+        <Button
+          type="button"
+          className="md:hidden"
+          size="icon"
+          variant="default"
+        >
+          <Mic />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
     </div>
   );
 }
